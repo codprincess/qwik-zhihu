5890357fedaa8f3c7257ee319e256ed111d285b9
Hedy <714195347@qq.com>
2023-02-26 23:59:34 +0800

    1

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,11 +1,10 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <ul>
-      <li> home</li>
-      <li> <Link href="/about"> About</Link></li>
+      <li>home</li>
+      <li>About</li>
     </ul>
   )
 })
--- src/routes/(account)/profile/index.tsx
+++ src/routes/(account)/profile/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      About
+      Profile
     </>
   )
 })
